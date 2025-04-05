dd678376abbff7ac761bbfd4ce1901bf561c2193
h4nsoo <mbhansoo05@gmail.com>
2025-04-05 01:15:45 +0100

    Final todo application

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -1,17 +1,53 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPenToSquare, faTrash } from "@fortawesome/free-solid-svg-icons";
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
-import { faTrash } from '@fortawesome/free-solid-svg-icons'
+export default function ToDo({ task, onToggle, onEdit, onDelete, updateTask }) {
 
-export default function ToDo({ task }) {
+  const [updatedName, setUpdatedName] = useState(task.name);
+
+  const handleUpdate = (e) => {
+    e.preventDefault();
+    updateTask(task.id, updatedName);
+  };
+
   return (
-    <div className='todo-list'>
-      <p>{task.name}</p>
-      <div>
-        <FontAwesomeIcon icon={faPenToSquare} className='edit' />
-        <FontAwesomeIcon icon={faTrash} className='delete' />
-      </div>
+    <div className="todo-task">
+      {task.isUpdating ? (
+        <form onSubmit={handleUpdate}>
+          <input
+            type="text"
+            value={updatedName}
+            onChange={(e) => setUpdatedName(e.target.value)}
+            className="todo-input"
+          />
+          <button type="submit" className="save-btn">
+            Save
+          </button>
+        </form>
+      ) : (
+        <>
+          <p
+            className={task.done ? "completed" : "incompleted"}
+            onClick={() => onToggle(task.id)}
+          >
+            {task.name}
+          </p>
+          <div>
+            <FontAwesomeIcon
+              icon={faPenToSquare}
+              className="edit"
+              onClick={() => onEdit(task)}
+            />
+            <FontAwesomeIcon
+              icon={faTrash}
+              className="delete"
+              onClick={() => onDelete(task.id)}
+            />
+          </div>
+        </>
+      )}
     </div>
-  )
+  );
 }
--- src/components/ToDoWrapper.jsx
+++ src/components/ToDoWrapper.jsx
@@ -2,29 +2,62 @@ import React from "react";
 import ToDoForm from "./ToDoForm";
 import ToDo from "./ToDo";
 import { useState } from "react";
-import {v4 as uuidv4} from "uuid";
-
+import { v4 as uuidv4 } from "uuid";
 
 export default function TodoWrapper() {
   const [tasks, setTasks] = useState([]);
 
   const addTask = (taskName) => {
-    setTasks([...tasks, {
-        id : uuidv4(),
-        name : taskName,
-        done : false,
-        isUpdating : false
-    }]);
+    setTasks([
+      ...tasks,
+      {
+        id: uuidv4(),
+        name: taskName,
+        done: false,
+        isUpdating: false,
+      },
+    ]);
   };
+
+  const onToggle = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, done: !task.done } : task
+      )
+    );
+  };
+
+
+  const onDelete = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  const onEdit = (task) => {
+    setTasks(
+      tasks.map((t) =>
+        t.id === task.id ? { ...t, isUpdating: !t.isUpdating } : t
+      )
+    );
+  };
+
+  
+const updateTask = (id, updatedName) => {
+  setTasks(
+    tasks.map((task) =>
+      task.id === id ? { ...task, name: updatedName, isUpdating: false } : task
+    )
+  );
+};
 
   return (
     <div className="todo-wrapper">
+      <center>
+        <h1>Get organized, Be efficient !</h1>
+      </center>
       <ToDoForm addTask={addTask} />
-      {
-        tasks.map((task) => (
-          <ToDo key={task.id} task={task} />
-        ))
-      }
+      {tasks.map((task) => (
+        <ToDo key={task.id} task={task} onToggle={onToggle} onDelete={onDelete} onEdit={onEdit} updateTask={updateTask} />
+      ))}
     </div>
   );
 }
